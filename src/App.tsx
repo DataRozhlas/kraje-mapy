3b61015e46c90d32bd589b0f8c62f8b3a46980b9
Petr Kočí <petr.koci@samizdat.cz>
2024-08-19 11:37:49 +0200

    skeleton

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className={"max-w-[620px] mx-auto"}>
+      ahoj
+    </div>
+  )
+}
+
+export default App
