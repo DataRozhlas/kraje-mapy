b2f494af9c3c8608dde06966ff203b5b1bc37bc8
Petr Kočí <petr.koci@samizdat.cz>
2024-10-30 13:08:15 +0100

    aggregate turnout by ORP

--- srv/aggregate-ucasti.ts
+++ srv/aggregate-ucasti.ts
@@ -1,9 +1,68 @@
 import { csvParse } from "d3-dsv";
 
-const ucastiData = await Bun.file("srv/data/2024/ucasti.csv").text();
-const ucasti = csvParse(ucastiData);
+const obecORPData = await Bun.file("srv/data/VAZ0065_0043_CS.csv").text();
+const obecORP = csvParse(obecORPData);
 
-const obecOkresData = await Bun.file("srv/data/VAZ0065_0043_CS.csv").text();
-const obecOkres = csvParse(obecOkresData);
+const ORPSData = await Bun.file("srv/data/VAZ0108_0065_CS.csv").text();
+const ORPS = csvParse(ORPSData);
 
-console.log(obecOkres);
+import obecResults2024 from "./data/obecResults2024.json";
+import obecResults2020 from "./data/obecResults2020.json";
+
+const result = ORPS.map((o) => {
+  return {
+    KRAJ: o.text1,
+    ORP: o.text2,
+    UCAST20: obecORP
+      .filter((obec) => obec.text1 === o.text2)
+      .map((obec) => obec.chodnota2)
+      .reduce(
+        (acc, curr) => {
+          const obceResults = obecResults2020.filter(
+            (obec) => obec.cisobec === curr && obec.typobec !== "MCMO"
+          );
+          acc.obvyd += obceResults.reduce(
+            (acc, curr) => acc + Number(curr.obvyd),
+            0
+          );
+          acc.volici += obceResults.reduce(
+            (acc, curr) => acc + Number(curr.volici),
+            0
+          );
+          return acc;
+        },
+        {
+          obvyd: 0,
+          volici: 0,
+        }
+      ),
+    UCAST24: obecORP
+      .filter((obec) => obec.text1 === o.text2)
+      .map((obec) => obec.chodnota2)
+      .reduce(
+        (acc, curr) => {
+          const obceResults = obecResults2024.filter(
+            (obec) => obec.cisobec === curr && obec.typobec !== "MCMO"
+          );
+          acc.obvyd += obceResults.reduce(
+            (acc, curr) => acc + Number(curr.obvyd),
+            0
+          );
+          acc.volici += obceResults.reduce(
+            (acc, curr) => acc + Number(curr.volici),
+            0
+          );
+          return acc;
+        },
+        { obvyd: 0, volici: 0 }
+      ),
+  };
+}).map((o) => {
+  return {
+    ...o,
+    UCAST20: Math.round((o.UCAST20.obvyd / o.UCAST20.volici) * 10000) / 100,
+    UCAST24: Math.round((o.UCAST24.obvyd / o.UCAST24.volici) * 10000) / 100,
+  };
+});
+
+console.log(result);
