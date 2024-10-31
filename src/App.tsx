6d0f9ca5688790eb1809aa7a9b1dd6a784377057
Petr Kočí <petr.koci@samizdat.cz>
2024-10-31 10:31:33 +0100

    embed id

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,56 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
 
+import ComboBox from "./components/ComboBox";
+import Map from "./components/Map";
 
-import ComboBox from './components/ComboBox'
-import Map from './components/Map'
+import { usePostMessageWithHeight } from "./hooks/usePostHeightMessage";
 
-import { usePostMessageWithHeight } from './hooks/usePostHeightMessage'
-
-
-function App({ kraj, index }: { kraj: string | null, index: string | null }) {
-  const [selectedKraj, setSelectedKraj] = useState(kraj || "")
+function App({ kraj }: { kraj: string | null }) {
+  const [selectedKraj, setSelectedKraj] = useState(kraj || "");
   // const selectedKraj = kraj || "";
-  const setIndex = index || "";
 
-  const [activeTooltip, setActiveTooltip] = useState("")
-  const { containerRef, postHeightMessage } = usePostMessageWithHeight(`map-${kraj}-${setIndex}`)
+  const [activeTooltip, setActiveTooltip] = useState("");
+  const { containerRef, postHeightMessage } =
+    usePostMessageWithHeight(`map-orp-difference`);
 
   useEffect(() => {
-    postHeightMessage()
-  }, [selectedKraj, containerRef, postHeightMessage])
-
+    postHeightMessage();
+  }, [selectedKraj, containerRef, postHeightMessage]);
 
   useEffect(() => {
     const searchParams = new URLSearchParams(window.location.search);
     if (selectedKraj) {
-      searchParams.set('kraj', selectedKraj);
+      searchParams.set("kraj", selectedKraj);
     } else {
-      searchParams.delete('kraj');
+      searchParams.delete("kraj");
     }
     const newUrl = `${window.location.pathname}?${searchParams.toString()}`;
-    window.history.pushState({}, '', newUrl);
+    window.history.pushState({}, "", newUrl);
   }, [selectedKraj]);
 
-
-
   return (
-    <div ref={containerRef} className={"max-w-[620px] mx-auto flex flex-col gap-6"}>
+    <div
+      ref={containerRef}
+      className={"max-w-[620px] mx-auto flex flex-col gap-6"}
+    >
       <div className="flex justify-between items-center">
         <ComboBox kraj={selectedKraj} setKraj={setSelectedKraj} />
         <div className="flex gap-1">
           <div className={"text-xs flex flex-col items-center gap-1"}>
-            <div className={"text-zinc-400"}>
-              &#x25BC;
-            </div>
-            <div>
-              &#x25BC;
-            </div>
+            <div className={"text-zinc-400"}>&#x25BC;</div>
+            <div>&#x25BC;</div>
           </div>
           <div className={"text-xs flex flex-col items-start gap-1"}>
-            <div>
-              průměr ČR
-            </div>
-            <div>
-              průměr kraje
-            </div>
+            <div>průměr ČR</div>
+            <div>průměr kraje</div>
           </div>
         </div>
       </div>
-      <div className={"flex flex-col gap-10 xs:flex-row xs:flex-wrap xs:gap-y-8 xs:gap-x-4"}>
+      <div
+        className={
+          "flex flex-col gap-10 xs:flex-row xs:flex-wrap xs:gap-y-8 xs:gap-x-4"
+        }
+      >
         {/* <div className={"xs:w-1/2"}>
           <Map kraj={selectedKraj} property={"CHUD"} activeTooltip={activeTooltip} setTooltip={setActiveTooltip} boundary={containerRef} />
         </div>
@@ -64,15 +58,26 @@ function App({ kraj, index }: { kraj: string | null, index: string | null }) {
           <Map kraj={selectedKraj} property={"ZNEV"} activeTooltip={activeTooltip} setTooltip={setActiveTooltip} boundary={containerRef} />
         </div> */}
         <div className={"xs:w-half-minus-2"}>
-          <Map kraj={selectedKraj} property={"KOALICE"} activeTooltip={activeTooltip} setTooltip={setActiveTooltip} boundary={containerRef} />
+          <Map
+            kraj={selectedKraj}
+            property={"KOALICE"}
+            activeTooltip={activeTooltip}
+            setTooltip={setActiveTooltip}
+            boundary={containerRef}
+          />
         </div>
         <div className={"xs:w-half-minus-2"}>
-          <Map kraj={selectedKraj} property={"OPOZICE"} activeTooltip={activeTooltip} setTooltip={setActiveTooltip} boundary={containerRef} />
+          <Map
+            kraj={selectedKraj}
+            property={"OPOZICE"}
+            activeTooltip={activeTooltip}
+            setTooltip={setActiveTooltip}
+            boundary={containerRef}
+          />
         </div>
       </div>
-
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
 function getURLParameter(name: string): string | null {
   return new URLSearchParams(window.location.search).get(name);
 }
 
-let kraj = getURLParameter('kraj');
-let index = getURLParameter('index');
+let kraj = getURLParameter("kraj");
 
-
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App kraj={kraj} index={index} />
-  </StrictMode>,
-)
+    <App kraj={kraj} />
+  </StrictMode>
+);
